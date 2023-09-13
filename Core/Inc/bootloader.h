83779d2114a306f181dd070a78408b00e9be7ec2
AbdelRhman Ibrahim <114269455+AbdelrhmanJaber@users.noreply.github.com>
2023-09-13 22:36:27 +0300

    v0.1

--- Core/Inc/bootloader.h
+++ Core/Inc/bootloader.h
@@ -20,6 +20,24 @@
 #define BL_hostCommunicationUART    &huart1
 
 
+#define CBL_GET_VER_CMD              0x10
+#define CBL_GET_HELP_CMD             0x11
+#define CBL_GET_CID_CMD              0x12
+/* Get Read Protection Status */
+#define CBL_GET_RDP_STATUS_CMD       0x13
+#define CBL_GO_TO_ADDR_CMD           0x14
+#define CBL_FLASH_ERASE_CMD          0x15
+#define CBL_MEM_WRITE_CMD            0x16
+/* Enable/Disable Write Protection */
+#define CBL_ED_W_PROTECT_CMD         0x17
+#define CBL_MEM_READ_CMD             0x18
+/* Get Sector Read/Write Protection Status */
+#define CBL_READ_SECTOR_STATUS_CMD   0x19
+#define CBL_OTP_READ_CMD             0x20
+/* Change Read Out Protection Level */
+#define CBL_CHANGE_ROP_Level_CMD     0x21
+
+
 void BL_Print_Message(char *format, ...);
 
 void BL_fetchHostCommand(void);
--- Core/Src/bootloader.c
+++ Core/Src/bootloader.c
@@ -14,7 +14,25 @@
 
 /****************** static function declaration ******************/
 
-
+static void Bootloader_Get_Version(uint8_t *copy_Puint8_hostBuffer);
+static void Bootloader_Get_Help(uint8_t *copy_Puint8_hostBuffer);
+static void Bootloader_Get_Chip_Identification_Number(uint8_t *copy_Puint8_hostBuffer);
+static void Bootloader_Read_Protection_Level(uint8_t *copy_Puint8_hostBuffer);
+static void Bootloader_Jump_To_Address(uint8_t *copy_Puint8_hostBuffer);
+static void Bootloader_Erase_Flash(uint8_t *copy_Puint8_hostBuffer);
+static void Bootloader_Memory_Write(uint8_t *copy_Puint8_hostBuffer);
+static void Bootloader_Enable_RW_Protection(uint8_t *copy_Puint8_hostBuffer);
+static void Bootloader_Memory_Read(uint8_t *copy_Puint8_hostBuffer);
+static void Bootloader_Get_Sector_Protection_Status(uint8_t *copy_Puint8_hostBuffer);
+static void Bootloader_Read_OTP(uint8_t *copy_Puint8_hostBuffer);
+static void Bootloader_Change_Read_Protection_Level(uint8_t *copy_Puint8_hostBuffer);
+static uint8_t Bootloader_CRC_Verify(uint8_t *copy_Puint8_Data, uint32_t copy_uint32_dataLenght,
+		uint32_t copy_uint32_hostCRC);
+static void Bootloader_Send_ACK(uint8_t copy_uint8_replayLenght);
+static void Bootloader_Send_NACK(void);
+static void Bootloader_Send_Data_To_Host(uint8_t *copy_Puint8_hostBuffer, uint32_t copy_uint32_dataLenght);
+static uint8_t Host_Address_Verification(uint32_t copy_uint32_jumpAddress);
+static uint8_t Perform_Flash_Erase(uint8_t copy_uint8_sectorNumber, uint8_t copy_uint8_numberOfSectors);
 
 /************************** variables and structures definition***************************************/
 
@@ -33,12 +51,56 @@ void BL_fetchHostCommand(void){
 	/*fetch the command from the user*/
 	HAL_UART_Receive(BL_hostCommunicationUART, &hostBuffer[1], dataLenght, HAL_MAX_DELAY);
 	switch(hostBuffer[1]){
-		case TEST_BOOTLOADER:{
-			BL_Print_Message("Bootloader is ok \r\n");
+		case CBL_GET_VER_CMD:{
+			BL_Print_Message("CBL_GET_VER_CMD \r\n");
+			break;
+		}
+		case CBL_GET_HELP_CMD:{
+			BL_Print_Message("CBL_GET_HELP_CMD \r\n");
+			break;
+		}
+		case CBL_GET_CID_CMD:{
+			BL_Print_Message("CBL_GET_CID_CMD \r\n");
+			break;
+		}
+		case CBL_GET_RDP_STATUS_CMD:{
+			BL_Print_Message("CBL_GET_RDP_STATUS_CMD \r\n");
+			break;
+		}
+		case CBL_GO_TO_ADDR_CMD:{
+			BL_Print_Message("CBL_GO_TO_ADDR_CMD \r\n");
+			break;
+		}
+		case CBL_FLASH_ERASE_CMD:{
+			BL_Print_Message("CBL_FLASH_ERASE_CMD \r\n");
+			break;
+		}
+		case CBL_MEM_WRITE_CMD:{
+			BL_Print_Message("CBL_MEM_WRITE_CMD \r\n");
+			break;
+		}
+		case CBL_ED_W_PROTECT_CMD:{
+			BL_Print_Message("CBL_ED_W_PROTECT_CMD \r\n");
+			break;
+		}
+		case CBL_MEM_READ_CMD:{
+			BL_Print_Message("CBL_MEM_READ_CMD \r\n");
+			break;
+		}
+		case CBL_READ_SECTOR_STATUS_CMD:{
+			BL_Print_Message("CBL_READ_SECTOR_STATUS_CMD \r\n");
+			break;
+				}
+		case CBL_OTP_READ_CMD:{
+			BL_Print_Message("CBL_OTP_READ_CMD \r\n");
+			break;
+				}
+		case CBL_CHANGE_ROP_Level_CMD:{
+			BL_Print_Message("CBL_CHANGE_ROP_Level_CMD \r\n");
 			break;
 		}
 		default:{
-			BL_Print_Message("wrong\r\n");
+			BL_Print_Message("Invalid Command\r\n");
 			break;
 		}
 	}
@@ -53,3 +115,87 @@ void BL_Print_Message(char *format, ...){
 	HAL_UART_Transmit(BL_hostCommunicationUART, (uint8_t *)Messsage, sizeof(Messsage), HAL_MAX_DELAY);
 	va_end(args);
 }
+
+
+/************************************ Implement static functions*********************************/
+
+
+
+
+static void Bootloader_Get_Version(uint8_t *copy_Puint8_hostBuffer){
+
+}
+
+static void Bootloader_Get_Help(uint8_t *copy_Puint8_hostBuffer){
+
+}
+
+static void Bootloader_Get_Chip_Identification_Number(uint8_t *copy_Puint8_hostBuffer){
+
+}
+
+static void Bootloader_Read_Protection_Level(uint8_t *copy_Puint8_hostBuffer){
+
+}
+
+static void Bootloader_Jump_To_Address(uint8_t *copy_Puint8_hostBuffer){
+
+}
+
+static void Bootloader_Erase_Flash(uint8_t *copy_Puint8_hostBuffer){
+
+}
+
+static void Bootloader_Memory_Write(uint8_t *copy_Puint8_hostBuffer){
+
+}
+
+static void Bootloader_Enable_RW_Protection(uint8_t *copy_Puint8_hostBuffer){
+
+}
+
+static void Bootloader_Memory_Read(uint8_t *copy_Puint8_hostBuffer){
+
+}
+
+static void Bootloader_Get_Sector_Protection_Status(uint8_t *copy_Puint8_hostBuffer){
+
+}
+
+static void Bootloader_Read_OTP(uint8_t *copy_Puint8_hostBuffer){
+
+}
+
+static void Bootloader_Change_Read_Protection_Level(uint8_t *copy_Puint8_hostBuffer){
+
+}
+
+static uint8_t Bootloader_CRC_Verify(uint8_t *copy_Puint8_Data, uint32_t copy_uint32_dataLenght,
+		uint32_t copy_uint32_hostCRC){
+
+}
+
+static void Bootloader_Send_ACK(uint8_t copy_uint8_replayLenght){
+
+}
+
+static void Bootloader_Send_NACK(void){
+
+}
+
+static void Bootloader_Send_Data_To_Host(uint8_t *copy_Puint8_hostBuffer, uint32_t copy_uint32_dataLenght){
+
+}
+
+static uint8_t Host_Address_Verification(uint32_t copy_uint32_jumpAddress){
+
+}
+
+static uint8_t Perform_Flash_Erase(uint8_t copy_uint8_sectorNumber, uint8_t copy_uint8_numberOfSectors){
+
+}
+
+
+
+
+
